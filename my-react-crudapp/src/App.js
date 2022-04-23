0d6cc1a6462b581e7b660717da9b7c1c179a4425
Luke Ryan <luke_t_ryan@hotmail.com>
2022-04-23 14:00:53 +1000

    updated npm react-scripts

--- my-react-crudapp/src/App.js
+++ my-react-crudapp/src/App.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 //import CreateUser from './CreateUser'
 export default function Navigate(){
    return (
-       <nav class="flex items-center justify-between flex-wrap bg-green-500 p-6">
-       <div class="flex items-center flex-shrink-0 text-white mr-6">
-           <span class="font-semibold text-xl tracking-tight">REACT CRUD APP</span>
+       <nav className="flex items-center justify-between flex-wrap bg-green-500 p-6">
+       <div className="flex items-center flex-shrink-0 text-white mr-6">
+           <span className="font-semibold text-xl tracking-tight">REACT CRUD APP</span>
        </div>
        <Link to="/">
-           <button class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">
+           <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">
                HOME
            </button>
        </Link>
--- my-react-crudapp/tailwind.config.js
+++ my-react-crudapp/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
   ],
   theme: {
     extend: {},
