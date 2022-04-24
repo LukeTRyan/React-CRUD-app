dc6588f83a81573eba292d1e44d5628090531be4
Luke Ryan <luke_t_ryan@hotmail.com>
2022-04-24 11:56:19 +1000

    added userlist

--- my-react-crudapp/src/App.js
+++ my-react-crudapp/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-//import { Route } from 'react-router-dom';
-//import CreateUser from './CreateUser'
+import UsersList from "./UsersList.js";
+
 export default function Navigate(){
    return (
-       <nav className="flex items-center justify-between flex-wrap bg-green-500 p-6">
-       <div className="flex items-center flex-shrink-0 text-white mr-6">
-           <span className="font-semibold text-xl tracking-tight">REACT CRUD APP</span>
+       <div className="main">
+            <nav className="flex items-center justify-between flex-wrap bg-green-500 p-6">
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                    <span className="font-semibold text-xl tracking-tight">REACT CRUD APP</span>
+                </div>
+                <Link to="/">
+                    <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">
+                        HOME
+                    </button>
+                </Link>
+            </nav>
+            <div className="usersList">
+                <UsersList/>
+            </div>
        </div>
-       <Link to="/">
-           <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">
-               HOME
-           </button>
-       </Link>
-       </nav>
    )
 }
+
+
